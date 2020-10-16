98420a15fa7508d503abf84c3dfe8cdae06b7cdd
Rohit Singh <r.singh@thesynapses.com>
2020-10-17 02:11:09 +0530

    Added Multithreading for faster Image Processing and download

--- downloadimage.py
+++ downloadimage.py
@@ -3,17 +3,15 @@
 # Email: rohitsingh119cs@gmail.com
 # -------------Developer-------------- #
 
-
+import concurrent.futures
 import requests
 from pathlib import Path
-
 
 # Path of the file contain URLS replace with urls.txt and provide the download location
 urls_file = open('urls.txt', 'r')
 urls = urls_file.readlines()
 download_file_path = "download/"
 logfile = open("logs.txt", "a")
-
 
 # Variables for logs file
 Total_url = 0
@@ -26,18 +24,21 @@ Total_downloaded_size = 0
 # Check the URL Content is Downloadable or not
 
 def is_downloadable(url):
-    h = requests.head(url, allow_redirects=True)
-    header = h.headers
-    content_type = header.get('content-type')
-    content_size = header.get('Content-Length')
-    print("File Size", int(content_size)/1000, "kb")
-    global Total_downloaded_size
-    Total_downloaded_size +=int(content_size)
-    if 'text' in content_type.lower():
+    try:
+        h = requests.head(url, allow_redirects=True)
+        header = h.headers
+        content_type = header.get('content-type')
+        content_size = header.get('Content-Length')
+        print("File Size", int(content_size) / 1000, "kb")
+        global Total_downloaded_size
+        Total_downloaded_size += int(content_size)
+        if 'text' in content_type.lower():
+            return False
+        if 'html' in content_type.lower():
+            return False
+        return True
+    except Exception as e:
         return False
-    if 'html' in content_type.lower():
-        return False
-    return True
 
 
 # Get the filename from the urls
@@ -51,7 +52,11 @@ def filename_from_url(url):
 
 
 # Iterate the files urls one by one from file and create the file
-for url in urls:
+def download_urls(url):
+    global Total_url
+    global already_available_file
+    global Downloaded_Count
+    global Not_Downloadable
     Total_url += 1
     if is_downloadable(url.strip()):
         filename = filename_from_url(url.strip())
@@ -69,19 +74,24 @@ for url in urls:
         logfile.write(url.strip())
         logfile.write("\n")
 
-logfile.write("###############################################################################\n" +
-              "Total URLs To be Downloaded = "+str(Total_url)+" Links"+'\n' +
-              "Total Images Link Not Downloadable = "+str(Not_Downloadable)+" Files"+'\n' +
-              "Total New Images Downloaded = "+str(Downloaded_Count)+" Files"+'\n' +
-              "Images Already Available or Downloaded in Folder = "+str(already_available_file)+" Files"+'\n' +
-              "Total Downloaded Size = "+str(round(int(Total_downloaded_size)/1048576, 2)) + "Mb"+"\n" +
-              "###############################################################################")
 
-print("-------------------------------------------------------------------------------")
-print("Total Images URLs = ", Total_url)
-print("Total Images Link Not Downloadable = ", Not_Downloadable)
-print("Total New Images Downloaded = ", Downloaded_Count)
-print("Downloaded Images Already Available in Folder = ", already_available_file)
-print("Downloaded Size = ", round(int(Total_downloaded_size)/1048576, 2), "Mb")
-print("-------------------------------------------------------------------------------")
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_urls, urls)
+
+logfile.write("###############################################################################\n" +
+              "Total URLs To be Downloaded = " + str(Total_url) + " Links" + '\n' +
+              "Total Images Link Not Downloadable = " + str(Not_Downloadable) + " Files" + '\n' +
+              "Total New Images Downloaded = " + str(Downloaded_Count) + " Files" + '\n' +
+              "Images Already Available or Downloaded in Folder = " + str(already_available_file) + " Files" + '\n' +
+              "Total Downloaded Size = " + str(round(int(Total_downloaded_size) / 1048576, 2)) + "Mb" + "\n" +
+              "###############################################################################\n")
+
+message = "-------------------------------------------------------------------------------\n"
+message += "Total Images URLs = " + str(Total_url) + "\n"
+message += "Total Images Link Not Downloadable = " + str(Not_Downloadable) + "\n"
+message += "Total New Images Downloaded = " + str(Downloaded_Count) + "\n"
+message += "Downloaded Images Already Available in Folder = " + str(already_available_file) + "\n"
+message += "Downloaded Size = " + str(round(int(Total_downloaded_size) / 1048576, 2))+ "Mb" + "\n"
+message += "-------------------------------------------------------------------------------" + "\n"
+print(message)
 logfile.close()
